c3452a5ec8042973ae7866746b9b4a715245c308
Estudiante <Estudiante@FI454402-07.unal.loc>
2016-08-09 14:58:32 -0500

    Clase POO

---
+++ src/clase/pkg3/CLASE3.java
@@ -0,0 +1,34 @@
+package clase.pkg3;
+
+/**
+ *
+ * @author Estudiante
+ */
+
+public class CLASE3 {
+
+    public static void main(String[] args) {
+        int x = 3;
+        while (x > 0) {
+            if (x > 2) {
+                System.out.print("a");
+            }
+            if (x == 2) {
+                System.out.print("b c");
+            }
+            if (x == 1) {
+                System.out.print("d");
+                x = x - 1;
+            }
+            x = x - 1;
+            System.out.print("-");
+        }
+        
+        int a = 0;
+        int y = 0;
+        while (x<5){
+            System.out.println(x 
+        }
+    }
+    
+}
